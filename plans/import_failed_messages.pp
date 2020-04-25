c4e2d7e8414618f1b61d65e135ca9eff0856d816
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-04-25 01:03:00 -0500

    WIP

--- plans/import_failed_messages.pp
+++ plans/import_failed_messages.pp
@@ -21,29 +21,44 @@ plan nservicebusservicecontrol::import_failed_messages (
   # X. Enable agent again
   # $target_objects = get_targets($targets)
   # C:\ProgramData\PuppetLabs\puppet\cache\state\agent_disabled.lock
-  run_task('agent_disenable', $targets, action => 'disable', message => 'Importing failed messages.')
-  run_task('service::windows', $targets, action => 'stop', name => $instance_name)
-    # Loop until items in the pipeline stage are done
-  ctrl::do_until( 'limit' => 5 ) || {
-    $results = run_task('service::windows', $targets, action => 'status', name => $instance_name)
-    $results.each |$result| {
-      if $result['status'] == 'Stopped' {
-        true
-      }
-      else {
-        false
+  get_targets($targets).each |$target| {
+    run_task('agent_disenable', $target, action => 'disable', message => 'Importing failed messages.')
+    ctrl::do_until( 'limit' => 20 ) || {
+      $results = run_command("Test-Path -Path 'C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_catalog_run.lock'", $target)
+      $results.each |$result| {
+        if $result['stdout'] == 'True' {
+          ctrl::sleep(10)
+          true
+        }
+        else {
+          false
+        }
       }
     }
-  }
-  ctrl::sleep(10)
-  if $instance_type == 'audit' {
-    $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl.Audit\\ServiceControl.Audit.exe"
-  } else {
-    $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl\\ServiceControl.exe"
-  }
+    run_task('service::windows', $targets, action => 'stop', name => $instance_name)
+      # Loop until items in the pipeline stage are done
+    ctrl::do_until( 'limit' => 5 ) || {
+      $results = run_task('service::windows', $targets, action => 'status', name => $instance_name)
+      $results.each |$result| {
+        if $result['status'] == 'Stopped' {
+          true
+        }
+        else {
+          false
+        }
+      }
+    }
+    # For some reason if you don't wait just a little bit servicecontrol
+    # will throw an unhandled exception on import...
+    ctrl::sleep(10)
+    if $instance_type == 'audit' {
+      $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl.Audit\\ServiceControl.Audit.exe"
+    } else {
+      $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl\\ServiceControl.exe"
+    }
 
-  run_command("& '${root_command}' --serviceName=${instance_name} --import-failed-${instance_type}s", $targets)
-  run_task('service::windows', $targets, action => 'start', name => $instance_name)
-  run_task('agent_disenable', $targets, action => 'enable')
-
+    run_command("& '${root_command}' --serviceName=${instance_name} --import-failed-${instance_type}s", $targets)
+    run_task('service::windows', $targets, action => 'start', name => $instance_name)
+    run_task('agent_disenable', $targets, action => 'enable')
+  }
 }
