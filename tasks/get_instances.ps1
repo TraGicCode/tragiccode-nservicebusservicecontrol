6cbe125be0d09a956c04cb947ea61db6e41963b8
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-05-19 23:58:43 -0500

    Improve bolt task user input handling for importing of failed error/audit messages

---
+++ tasks/get_instances.ps1
@@ -0,0 +1,38 @@
+Import-Module "C:\Program Files (x86)\Particular Software\ServiceControl Management\ServiceControlMgmt.psd1" 4>$null
+
+$result = @()
+
+$errorInstances = Get-ServiceControlInstances
+foreach($instance in $errorInstances)
+{
+    $result += [PSCustomObject] @{
+        Name        = $instance.Name
+        InstallPath = $instance.InstallPath
+        ExecutablePath = Join-Path -Path $instance.InstallPath -ChildPath "ServiceControl.exe"
+        Type = 'error'
+    }
+}
+
+$auditInstances = Get-ServiceControlAuditInstances
+foreach($instance in $auditInstances)
+{
+    $result += [PSCustomObject] @{
+        Name        = $instance.Name
+        InstallPath = $instance.InstallPath
+        ExecutablePath = Join-Path -Path $instance.InstallPath -ChildPath "ServiceControl.Audit.exe"
+        Type = 'audit'
+    }
+}
+
+$monitoringInstances = Get-MonitoringInstances
+foreach($instance in $monitoringInstances)
+{
+    $result += [PSCustomObject] @{
+        Name        = $instance.Name
+        InstallPath = $instance.InstallPath 
+        ExecutablePath = Join-Path -Path $instance.InstallPath -ChildPath "ServiceControl.Monitoring.exe"
+        Type = 'monitor'
+    }
+}
+
+ConvertTo-Json -InputObject @{"_items" = $result} -Compress 
