bd4a924438c247ebb6cb0c30dea2b7dae7f0c1ed
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2020-05-19 19:07:24 -0500

    WIP

--- plans/import_failed_messages.pp
+++ plans/import_failed_messages.pp
@@ -5,11 +5,29 @@
 # @param instance_type The servicecontrol instance type (Audit or Error).
 plan nservicebusservicecontrol::import_failed_messages (
   TargetSpec $targets,
-  String $instance_name,
+  String[1] $instance_name,
   Enum['error', 'audit'] $instance_type,
 ) {
 
   get_targets($targets).each |$target| {
+    # Cannot trust user input so lets validate their data instead
+    # Maybe perform more validations?
+    out::message('Probing system for specified servicecontrol instance')
+    $instance_details_result = run_task('nservicebusservicecontrol::get_instances', $target)
+    $items = $instance_details_result.map |$r| { $r['_items'] }
+    $found_instance = $items.reduce([]) |$agg, $res| {
+      $agg + $res.filter |$instance| { $instance["Name"] == $instance_name }
+    }
+
+    if $found_instance.empty() {
+      fail_plan("A servicecontrol instance with the name of '${instance_name}' and type of '${instance_type}' could not be found.", 'nservicebusservicecontrol/servicecontrol-instance-not-found')
+    }
+    out::message("Probing Complete.  Found the servicecontrol instance with the name of '${instance_name}' and type of '${instance_type}'.")
+    # Shortcircuit if monitoring instance
+    if $found_instance[0]['Type'] == 'monitor' {
+      out::message('Importing of failed messages cannot be done for monitoring instances.')
+    }
+
     run_task('agent_disenable', $target, action => 'disable', message => 'Importing failed messages.')
     ctrl::do_until( 'limit' => 20 ) || {
       $results = run_command("Test-Path -Path 'C:\\ProgramData\\PuppetLabs\\puppet\\cache\\state\\agent_catalog_run.lock'", $target)
@@ -23,9 +41,9 @@ plan nservicebusservicecontrol::import_failed_messages (
         }
       }
     }
-    run_task('service::windows', $targets, action => 'stop', name => $instance_name)
+    run_task('service::windows', $target, action => 'stop', name => $instance_name)
     ctrl::do_until( 'limit' => 5 ) || {
-      $results = run_task('service::windows', $targets, action => 'status', name => $instance_name)
+      $results = run_task('service::windows', $target, action => 'status', name => $instance_name)
       $results.each |$result| {
         if $result['status'] == 'Stopped' {
           true
@@ -38,14 +56,9 @@ plan nservicebusservicecontrol::import_failed_messages (
     # For some reason if you don't wait just a little bit servicecontrol
     # will throw an unhandled exception on import...
     ctrl::sleep(10)
-    if $instance_type == 'audit' {
-      $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl.Audit\\ServiceControl.Audit.exe"
-    } else {
-      $root_command = "C:\\Program Files (x86)\\Particular Software\\Particular.ServiceControl\\ServiceControl.exe"
-    }
 
-    run_command("& '${root_command}' --serviceName=${instance_name} --import-failed-${instance_type}s", $targets)
-    run_task('service::windows', $targets, action => 'start', name => $instance_name)
-    run_task('agent_disenable', $targets, action => 'enable')
+    run_command("& '${found_instance[0]['ExecutablePath']}' --portable --serviceName=${found_instance[0]['Name']} --import-failed-${found_instance[0]['Type']}s", $target)
+    run_task('service::windows', $target, action => 'start', name => $instance_name)
+    run_task('agent_disenable', $target, action => 'enable')
   }
 }
